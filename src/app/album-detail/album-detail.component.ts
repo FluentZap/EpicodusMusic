20b882eb8c70bf097b034c5b379e86f501751009
dustin herboldshimer and todd aden <todd@>
2019-06-19 09:27:48 -0700

    add firebase and integrate for album storage

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-album-detail',
@@ -12,8 +13,8 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
-  albumId: number;
-  albumToDisplay: Album;
+  albumId: string;
+  albumToDisplay;
 
   constructor(
     private route: ActivatedRoute, 
@@ -23,9 +24,10 @@ export class AlbumDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
-      this.albumId = parseInt(urlParameters['id']);
+      this.albumId = urlParameters['id'];
     }); 
-    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);    
+
   }
 
 }
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 
 @Component({
@@ -14,11 +15,11 @@ import { AlbumService } from '../album.service';
 
 export class MarketplaceComponent implements OnInit {
   constructor(private router: Router, private albumService: AlbumService){}
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
   }
   goToDetailPage(clickedAlbum: Album) {
-    this.router.navigate(['albums', clickedAlbum.id]);
+    this.router.navigate(['albums', clickedAlbum.$key]);
   };
 }
